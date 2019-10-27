9204337a31d1eda2de45d4b6014cfaff8883ed5e
vladgo777 <vladgo777@gmail.com>
2019-10-27 13:50:40 +0300

    last

---
+++ build/js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+//slider slick
+  $('.slider').slick({
+  	dots: true,
+  	autoplay: true,
+  	slidesToShow: 1,
+  	slidesToScroll: 1,
+    nextArrow: $('#next'),
+    prevArrow: $('#prev')
+  });
+//scrollto
+      $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
+//order popup
+	$('.order').magnificPopup({
+		items: {
+          src: '#text-popup'
+        },
+        type:'inline'
+    });
+
+    $('.selector').click(function(event) {
+	    var value = $(this).attr('id');
+	    $('.input-selector').val(value);
+	});
+
+
+});
+ $(window).on('load', function() {
+   $('.imgLoad').fadeIn(2000);
+   $('.preloader').delay(2500).fadeOut('slow');
+  });
